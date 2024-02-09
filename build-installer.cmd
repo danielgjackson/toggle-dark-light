9b6cbf53973bc2f7ab3ba6bd20efaa99c135a68c
Daniel Jackson <email@danieljackson.co.uk>
2024-02-09 18:47:26 +0000

    Sign the executable

--- build-installer.cmd
+++ build-installer.cmd
@@ -27,7 +27,41 @@ for /f "tokens=3 usebackq" %%f in (`type toggle.rc ^| findstr /R /C:"#define[ ]V
 for /f "tokens=3 usebackq" %%f in (`type toggle.rc ^| findstr /R /C:"#define[ ]VER_BUILD"`) do set VER=%VER%.%%f
 rem for /f "tokens=3 usebackq" %%f in (`type toggle.rc ^| findstr /R /C:"#define[ ]VER_REVISION"`) do set VER=%VER%.%%f
 
-::: Using .wixproj for WiX V4
+::: Find signtool
+:PATH_WINDOWSKIT
+SET WINDOWSKIT_PATH=
+SET FIND_WINDOWSKIT=
+FOR %%p IN (signtool.exe) DO SET "FIND_WINDOWSKIT=%%~$PATH:p"
+IF NOT DEFINED FIND_WINDOWSKIT GOTO LOCATE_WINDOWSKIT
+SET WIX_PATH=%FIND_WINDOWSKIT:~0,-12%
+ECHO Windows Kit located on path: %WINDOWSKIT_PATH%
+GOTO WINDOWSKIT_FOUND
+
+:LOCATE_WINDOWSKIT
+ECHO Searching for Windows Kit...
+FOR /F "usebackq tokens=*" %%f IN (`DIR /B /ON "%ProgramFiles(x86)%\Windows Kits\10\bin\*"`) DO IF EXIST "%ProgramFiles(x86)%\Windows Kits\10\bin\%%f\x64\signtool.exe" SET "WINDOWSKIT_PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\%%f\x64\"
+IF "%WINDOWSKIT_PATH%"=="" ECHO Cannot find Windows Kit & GOTO ERROR
+ECHO Windows Kit found at: %WINDOWSKIT_PATH%
+
+:WINDOWSKIT_FOUND
+::: "%WINDOWSKIT_PATH%MakeCert.exe" -$ individual -pe -n "CN=danielgjackson" -sv D:\Certificates\mycert.pvk D:\Certificates\mycert.cer
+::: "%WINDOWSKIT_PATH%Pvk2Pfx.exe" -pvk D:\Certificates\mycert.pvk -spc D:\Certificates\mycert.cer -pfx D:\Certificates\mycert.pfx
+
+SET TIMESTAMP_SERVER=
+SET TIMESTAMP_SERVER=/t http://timestamp.digicert.com
+::: SET TIMESTAMP_SERVER=/t http://timestamp.verisign.com/scripts/timstamp.dll
+::: SET TIMESTAMP_SERVER=/t http://timestamp.globalsign.com/scripts/timstamp.dll
+::: SET TIMESTAMP_SERVER=/t http://timestamp.comodoca.com/authenticode
+::: SET TIMESTAMP_SERVER=/t http://www.startssl.com/timestamp
+::: SET TIMESTAMP_SERVER=/t http://timestamp.sectigo.com
+::: SET TIMESTAMP_SERVER=/t http://tsa.starfieldtech.com
+::: SET TIMESTAMP_SERVER=/t http://timestamp.digicert.com?alg=sha1
+
+::: Sign the executable
+"%WINDOWSKIT_PATH%signtool.exe" sign /debug /v /a /f "D:\Certificates\mycert.pfx" /d "Toggle Light/Dark" %TIMESTAMP_SERVER% "toggle.exe"
+IF ERRORLEVEL 1 GOTO ERROR
+
+::: Build the installer -- using .wixproj for WiX V4
 dotnet build -c Release toggle.wixproj
 IF ERRORLEVEL 1 (
   ECHO If WiX Toolset not installed:
@@ -39,35 +73,7 @@ IF ERRORLEVEL 1 (
 )
 copy /Y bin\Release\toggle.msi toggle.msi
 
-:PATH_WINDOWSKIT
-SET WINDOWSKIT_PATH=
-SET FIND_WINDOWSKIT=
-FOR %%p IN (signtool.exe) DO SET "FIND_WINDOWSKIT=%%~$PATH:p"
-IF NOT DEFINED FIND_WINDOWSKIT GOTO LOCATE_WINDOWSKIT
-SET WIX_PATH=%FIND_WINDOWSKIT:~0,-12%
-ECHO Windows Kit located on path: %WINDOWSKIT_PATH%
-GOTO SIGN
-
-:LOCATE_WINDOWSKIT
-ECHO Searching for Windows Kit...
-FOR /F "usebackq tokens=*" %%f IN (`DIR /B /ON "%ProgramFiles(x86)%\Windows Kits\10\bin\*"`) DO IF EXIST "%ProgramFiles(x86)%\Windows Kits\10\bin\%%f\x64\signtool.exe" SET "WINDOWSKIT_PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\%%f\x64\"
-IF "%WINDOWSKIT_PATH%"=="" ECHO Cannot find Windows Kit & GOTO ERROR
-ECHO Windows Kit found at: %WINDOWSKIT_PATH%
-
-:SIGN
-::: "%WINDOWSKIT_PATH%MakeCert.exe" -$ individual -pe -n "CN=danielgjackson" -sv D:\Certificates\mycert.pvk D:\Certificates\mycert.cer
-::: "%WINDOWSKIT_PATH%Pvk2Pfx.exe" -pvk D:\Certificates\mycert.pvk -spc D:\Certificates\mycert.cer -pfx D:\Certificates\mycert.pfx
-
-SET TIMESTAMP_SERVER=
-::: SET TIMESTAMP_SERVER=/t http://timestamp.verisign.com/scripts/timstamp.dll
-::: SET TIMESTAMP_SERVER=/t http://timestamp.globalsign.com/scripts/timstamp.dll
-::: SET TIMESTAMP_SERVER=/t http://timestamp.comodoca.com/authenticode
-::: SET TIMESTAMP_SERVER=/t http://www.startssl.com/timestamp
-::: SET TIMESTAMP_SERVER=/t http://timestamp.sectigo.com
-::: SET TIMESTAMP_SERVER=/t http://tsa.starfieldtech.com
-::: SET TIMESTAMP_SERVER=/t http://timestamp.digicert.com?alg=sha1
-
-
+::: Sign the installer
 rem "%WIX_PATH%insignia.exe" -ib "toggle.msi" -o "engine.exe"
 rem IF ERRORLEVEL 1 GOTO ERROR
 rem "%WINDOWSKIT_PATH%signtool.exe" sign /f "D:\Certificates\mycert.pfx" /d "Toggle Dark/Light Setup" %TIMESTAMP_SERVER% /v "engine.exe"
@@ -78,7 +84,9 @@ rem IF ERRORLEVEL 1 GOTO ERROR
 IF ERRORLEVEL 1 GOTO ERROR
 
 ECHO.
-ECHO Done: V%VER%
+DIR toggle.exe toggle.msi | FINDSTR /C:" toggle."
+ECHO.
+ECHO Done: %VER%
 IF DEFINED INTERACTIVE_IBUILD COLOR 2F & PAUSE & COLOR
 GOTO :EOF
 
