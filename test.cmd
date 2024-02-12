2b928d43db281574d46562feade950b1994f4738
Daniel Jackson <email@danieljackson.co.uk>
2024-02-12 17:43:30 +0000

    Fix to not run if build failed

--- test.cmd
+++ test.cmd
@@ -1,1 +1,7 @@
-@.\build.cmd && .\toggle.exe /CONSOLE:ATTACH /TOGGLE /EXIT
+@echo off
+call .\build.cmd
+if errorlevel 1 (
+    echo ERROR: Not running, build failed.
+    goto :eof
+)
+.\toggle.exe /CONSOLE:ATTACH /TOGGLE /EXIT
